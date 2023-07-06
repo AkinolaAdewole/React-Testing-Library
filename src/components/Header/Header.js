95f707824ef066b49ac438c46e13478a41825cc0
Akinola <adewoleakinola24@gmail.com>
2023-07-06 18:24:27 +0100

    findBy

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,7 @@ export default function Header({
         <>
             <h1 title="Header" className="header">{title}</h1>
             <h3  className="header" name="My Header">Hello</h3>
+            <h3  className="heading" name="My Header" data-testid='Olaide'>Olaide</h3>
         </>
     )
 }
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -24,9 +24,38 @@ import Header from './Header'
 //     expect(headingElement).toBeInTheDocument();
 // })
 
-test('should render same text passed into title prop', ()=>{
+test('should render getBy Role', ()=>{
     // The header is going to take in a prop, I can also add name if I have two header
     render(<Header title="My Header"/>) 
     const headingElement= screen.getByRole('heading',{name:'My Header'})
     expect(headingElement).toBeInTheDocument();
-})
+}) 
+
+test('should render same text passed into title prop', ()=>{
+    // The header is going to take in a prop, I can also add name if I have two header
+    render(<Header title="My Header"/>) 
+    const headingElement= screen.getByTitle('Header')
+    expect(headingElement).toBeInTheDocument();
+}) 
+
+
+
+test('should find element by data-testid', ()=>{
+    // The header is going to take in a prop, I can also add name if I have two header
+    render(<Header title="My Header"/>) 
+    const headingElement= screen.getByTestId('Olaide')
+    expect(headingElement).toBeInTheDocument();
+}) 
+
+
+
+//findBy
+// In React Testing Library, the findBy function is used to asynchronously find an element within the DOM tree based on a specific query. 
+// It returns a promise that resolves when the element is found or rejects if the element is not found within the specified timeout.
+
+test('example test', async () => {
+    render(<Header />);
+    const element = await screen.findByText('');
+    expect(element).toBeInTheDocument()
+    // Assertions or further actions with the found element
+  });
