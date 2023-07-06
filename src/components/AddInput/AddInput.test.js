dcd4049188a1db8bea50b25b58f90fb966076e81
Akinola <adewoleakinola24@gmail.com>
2023-07-06 21:59:08 +0100

    Fire Event

--- src/components/AddInput/AddInput.test.js
+++ src/components/AddInput/AddInput.test.js
@@ -1,10 +1,10 @@
-import {screen,render} from '@testing-library/react'
+import {screen,render, fireEvent} from '@testing-library/react'
 import AddInput from './AddInput'
 
 
 const mockSetTodo= jest.fn()
 describe('Add Input',()=>{
-    test('should render same text passed into title prop', async ()=>{
+    test('should render input element', async ()=>{
         // The header is going to take in a prop
         render(<AddInput 
             todos={[]}
@@ -12,4 +12,15 @@ describe('Add Input',()=>{
         const headingElement= screen.getByPlaceHolderText(/Add a new task here .../i)
         expect(headingElement).toBeInTheDocument();
     })
+
+    test('should be able to type into input', async ()=>{
+        // The header is going to take in a prop
+        render(<AddInput 
+            todos={[]}
+            setTodos={mockSetTodo} />)
+        const headingElement= screen.getByPlaceHolderText(/Add a new task here .../i)
+        fireEvent.change(inputElement, {value:"Go Grocery Shopping"})
+        expect(headingElement.value).toBe("Go Grocery Shopping");
+    })
+    
 })
