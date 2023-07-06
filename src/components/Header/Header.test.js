90e3fb3b84bf60bb7077fd22c49df4b174cb2d7a
Akinola <adewoleakinola24@gmail.com>
2023-07-06 18:05:38 +0100

    getBy

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -7,9 +7,26 @@ import Header from './Header'
 // Unit tests verify that these units of code work as expected, with specific inputs and outputs, without relying on external dependencies.
 
 
-test('should render same text passed into title prop',()=>{
-    // The header is going to take in a prop
-    render(<Header title="My Header" />)
-    const headingElement= screen.getByText(/My Header/i)
+//
+
+// test('should render same text passed into title prop', async ()=>{
+//     // The header is going to take in a prop
+//     render(<Header title="My Header" />)
+//     const headingElement= screen.getByTitle(/My Header/i)
+//     expect(headingElement).toBeInTheDocument();
+// })
+
+
+// test('should render same text passed into title prop', async ()=>{
+//     // The header is going to take in a prop, I can also add name if I have two header
+//     render(<Header title="My Header"/>)
+//     const headingElement= screen.getByRole('heading')
+//     expect(headingElement).toBeInTheDocument();
+// })
+
+test('should render same text passed into title prop', ()=>{
+    // The header is going to take in a prop, I can also add name if I have two header
+    render(<Header title="My Header"/>) 
+    const headingElement= screen.getByRole('heading',{name:'My Header'})
     expect(headingElement).toBeInTheDocument();
 })
