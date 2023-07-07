85276b70453198a2ac0fa4074afd4b2309542956
Akinola <adewoleakinola24@gmail.com>
2023-07-07 16:23:18 +0100

    Integration Test

--- src/components/AddInput/AddInput.test.js
+++ src/components/AddInput/AddInput.test.js
@@ -2,6 +2,10 @@ import {screen,render, fireEvent} from '@testing-library/react'
 import AddInput from './AddInput'
 
 
+// Integration tests in React with TypeScript involve testing the interaction and behavior of multiple components and their integration with each other. 
+// These tests focus on ensuring that the components work correctly together, simulating user actions, and verifying the expected outcomes.
+
+
 const mockSetTodo= jest.fn()
 describe('Add Input',()=>{
     test('should render input element', async ()=>{
